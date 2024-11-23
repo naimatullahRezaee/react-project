b4b8f96f96a8e77baa590d34a2bd7783938c5cd5
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-11-23 19:34:51 +0430

    add project section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,8 @@ import { assets } from "../assets/assets";
 const About = () => {
   return (
     <div
-      className="flex flex-col items-center justify-center container mx-auto  p-14 md:px-20 lg:px-32 w-full overflow-hidden"
       id="About"
+      className="flex flex-col items-center justify-center container mx-auto  p-14 md:px-20 lg:px-32 w-full overflow-hidden"
     >
       <h1 className="text-2xl sm:text-4xl font-bold mb-2 ">
         About{" "}
