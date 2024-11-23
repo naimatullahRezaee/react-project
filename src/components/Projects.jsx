05b3b9dd73dc14a50b8bf549e96c00906010bc75
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-11-23 19:50:27 +0430

    add image and description to project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { assets } from "../assets/assets";
+import { assets, projectsData } from "../assets/assets";
 
 const Projects = () => {
   return (
@@ -31,6 +31,30 @@ const Projects = () => {
         >
           <img src={assets.right_arrow} alt="Next" />
         </button>
+      </div>
+      {/* project slider container */}
+      <div className="overflow-hidden">
+        <div className="flex gap-8 transition-transform duration-500 ease-in-out">
+          {projectsData.map((project, index) => (
+            <div key={index} className="relative flex-shrink-0 w-full sm:w-1/4">
+              <img
+                src={project.image}
+                alt={project.title}
+                className="w-full h-auto mb-14"
+              />
+              <div className="absolute left-0 right-0 bottom-5 flex justify-center">
+                <div className="inline-block bg-white w-3/4 px-4 py-2 shadow-md ">
+                  <h2 className="text-xl font-semibold text-gray-500">
+                    {project.title}
+                  </h2>
+                  <p className="text-gray-500 text-sm">
+                    {project.price} <span>|</span> {project.location}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
